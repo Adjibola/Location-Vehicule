9bc0e28b2ea78833c4d3c2be76bdb91c60cf3b79
Adjibola <bakaryadjibola@gmail.com>
2021-12-03 03:10:17 -0500

    mon premier commit

---
+++ accueil.php
@@ -0,0 +1,7 @@
+<?php require'header.php';?>
+<div class="container">
+    <h1 class="mt-5">Location de vehicule</h1>
+    Envie de reserver un vehicule!Vous etes au bon endroit.
+Vous etes nouveau? <a href="/Location Vehicule/register.php">Cliquez ici</a>
+ </div>
+<?php require'footer.php';?>
